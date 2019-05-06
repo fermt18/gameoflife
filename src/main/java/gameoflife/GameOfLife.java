1a9f9182de815be97aabf3f8f69695175e5080e0
Ferran Montes <ferran.montes@mango.com>
2019-05-06 15:52:38 +0200

    added missing source files

---
+++ src/main/java/gameoflife/GameOfLife.java
@@ -0,0 +1,12 @@
+package gameoflife;
+
+public class GameOfLife {
+
+    public GameOfLife(boolean[][] board){
+
+    }
+
+    public boolean[][] getBoard(){
+        return new boolean[][]{};
+    }
+}
